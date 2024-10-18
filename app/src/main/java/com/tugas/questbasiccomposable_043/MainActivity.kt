f7cc5a1f6dbe4fe0432d96f51921327410862a66
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-18 16:40:46 +0700

    commit 4 penambahan image dan styling

--- app/src/main/java/com/tugas/questbasiccomposable_043/MainActivity.kt
+++ app/src/main/java/com/tugas/questbasiccomposable_043/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -55,11 +58,15 @@ fun BasicCompose(modifier: Modifier = Modifier) {
             fontWeight = FontWeight.Bold
         )
         Text(
-            text = "Ini Halaman Login",
+            text = "The Great Country",
             modifier = modifier,
             fontSize = 20.sp,
             color = Color.Black,
             fontWeight = FontWeight.Bold
+        )
+        Image(painter = painterResource(id = R.drawable.indo),
+            contentDescription = null,
+            modifier = Modifier.size(170.dp),
         )
     }
 }
