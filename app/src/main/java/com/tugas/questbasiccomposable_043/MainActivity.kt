21df7da390caed78c6f0bffd98a3730fa13ffb11
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-18 15:42:19 +0700

    commit 2 pembuatan fungsi BasicCompose dengan isi text Login

--- app/src/main/java/com/tugas/questbasiccomposable_043/MainActivity.kt
+++ app/src/main/java/com/tugas/questbasiccomposable_043/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.tugas.questbasiccomposable_043.ui.theme.QuestBasicComposable_043Theme
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +27,31 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestBasicComposable_043Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    BasicCompose(
+                        Fauzan = Modifier.padding(innerPadding).fillMaxSize(),
                     )
+
                 }
             }
         }
     }
 }
+@preview(showBackground = true)
+@Composable
+fun BasicCompose(Fauzan: Modifier = Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Login" , modifier = Fauzan,
+            fontSize = 60.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold)
+
+    }
+}
+
+annotation class preview(val showBackground: Boolean)
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
