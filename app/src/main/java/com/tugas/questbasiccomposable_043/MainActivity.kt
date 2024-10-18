8e45413644460796af3d1a796ee94c364a9412e3
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2024-10-19 02:18:49 +0700

    commit 10 penambahan fungsi circleshape ke foto 2

--- app/src/main/java/com/tugas/questbasiccomposable_043/MainActivity.kt
+++ app/src/main/java/com/tugas/questbasiccomposable_043/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -89,10 +93,13 @@ fun BasicCompose(modifier: Modifier = Modifier) {
             color = Color.Black,
             fontWeight = FontWeight.Bold,
         )
-        Image(painter = painterResource(id = R.drawable.ojan),
+        Image(
+            painter = painterResource(id = R.drawable.ojan),
             contentDescription = null,
-            modifier = Modifier.size(170.dp),
-            )
+            modifier = Modifier
+                .size(300.dp)
+                .clip(CircleShape),
+        )
     }
 }
 
